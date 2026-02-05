81cd6dc21d0427ce2364277006528a2b7a12ef99
xenreal <dbk.7219@gmail.com>
2026-02-05 13:54:12 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,19 +5,12 @@ const Projects = () => {
   return (
     <div className='p-4 md:p-8'>
       
-      {/* Title Section 
-          Removed 'pointer-events-none' since it's no longer overlapping
-      */}
       <div className='pt-[15vh] md:pt-[25vh] mb-10 md:mb-16 relative'>
          <h2 className='font-[font1] uppercase font-bold text-black text-[12vw] md:text-[8.5vw] leading-none'>
            Projects
          </h2>
       </div>
-
-      {/* Projects Section
-          REMOVED -mt-20 to prevent overlap.
-          Added pb-20 to ensure there is space at the bottom of the page.
-      */}
+      
       <div className=' pb-20'>
        <ProCon/>
       </div>
